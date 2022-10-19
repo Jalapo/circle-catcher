d5bb5f4ab9e3a4d354ce544b514a5a0da5d9696d
Jalapo <javenporter@gmail.com>
2022-10-19 12:57:57 -0500

    Changes & fixes to circle animations + the removal of their DOM elements
    Autonomized removal of individual circles after 1.5s.
    Fixed circle elements persisting after being removed.
    Fixed circles persisting after a game session ends.
    Circles now smoothly fade away as they disappear.
    Removed old/unused lines of code.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*
-general game layout
-drawing random circles within frame
-60s timer
-game
+: general game layout
+: drawing random circles within frame
+: 60s timer
+: game
 difficulties
 ui
 */
@@ -14,54 +14,74 @@ let scoreTxt;
 let gameScreen;
 let screenProps = {};
 let initTime = Date.now();
-let displayTime = 200; // 1/10 seconds (displayTime:100 = 10s)
+let displayTime = !window.location.href.includes("127.0.0.1") ? 600 : 50;
+// 1/10 seconds (displayTime:100 = 10s) || in dev., timer runs for 5s instead of 60
 let score = 0;
 let circles = [];
 let lags = [];
 
+
 window.onload = () => {
 //  start setup/draw layout
-    app = $('#CC_game_App'); // This ID is dedicated to the app container.
-    topBar = $("<div></div>").prop("id" ,'CC_game_topBar');
-    timeTxt = $("<span></span>").prop("id" ,'CC_game_timeTxt').text(`Time: ${Math.ceil(displayTime/10)}s`);
-    scoreTxt = $("<span></span>").prop("id" ,'CC_game_scoreTxt').text(`Score: ${score}`);
-    gameScreen = $("<div></div>").prop("id", "CC_game_Screen");
+app = $('#CC_game_App'); // This ID is dedicated to the app container.
+topBar = $("<div></div>").prop("id" ,'CC_game_topBar');
+timeTxt = $("<span></span>").prop("id" ,'CC_game_timeTxt').text(`Time: ${Math.ceil(displayTime/10)}s`);
+scoreTxt = $("<span></span>").prop("id" ,'CC_game_scoreTxt').text(`Score: ${score}`);
+gameScreen = $("<div></div>").prop("id", "CC_game_Screen");
 
-    topBar.append(timeTxt, scoreTxt); // add time and score to Top Bar
-    app.append(topBar, gameScreen); // add Top Bar and Screen to app container
-    gameScreen.empty();
-    updateScreen();
-
+topBar.append(timeTxt, scoreTxt); // add time and score to Top Bar
+app.append(topBar, gameScreen); // add Top Bar and Screen to app container
+updateScreen();
 // end draw layout
 
 // begin drawing circles
-    drawCircles = repeatFunc(()=> {
-        // if timer != 0 (game is running), draw a circle every ~400ms
-        if (displayTime) {createCircle(); return true;} else {return false;} 
-    }, 400, 0);
-// begin Game/BG timer
-    timer = repeatFunc(() => {
-        if (displayTime) {
-            circles.forEach(c => {if (Date.now() - c.createTime>1500) c.obj.remove();}); // remove old circles
-            displayTime--;
-            timeTxt.text(`Time: ${Math.ceil(displayTime/10)}s`);
-            return true;
-        } else {
-            circles.forEach(c => {c.obj.remove();})
-            return false;
+drawCircles = repeatFunc(()=> {
+    // if timer != 0 (game is running), draw a circle every ~400ms
+    if (displayTime) {createCircle(); return true;} else {return false;} 
+}, 400, 0);
+// begin game timer
+timer = repeatFunc(() => {
+    if (displayTime) {
+        displayTime--;
+        timeTxt.text(`Time: ${Math.ceil(displayTime/10)}s`);
+        return true;
+    } else {
+        return false;
+    }
+}, 100, 0);
+// begin cleanup timer
+clearOld = repeatFunc(() => {
+    if (displayTime) {
+        if (circles.length) {
+            circles.forEach(async(c) => {
+                if (Date.now() - c.createTime >= 1500 && !c.clicked) {
+                    c.clicked = true; // prevent a disappearing circle from having anything further ran on it
+                    let elm = gameScreen.find(c.obj); // find the element that matches the 'circles' array object
+                    elm.css("animation", "fadeOut 0.25s both");
+                    await wait(250);
+                    elm.remove();
+                }
+            });
         }
-    }, 100, 0);
+        return true;
+    } else {
+        for (let x=0;x<circles.length;x++) {circles[x].obj.css("animation", "fadeOut 0.25s both");}
+        setTimeout(()=>{gameScreen.empty();}, 300);
+        return false;
+    }
+}, 500, 0);
 }
 
 
+// --------------------------------------------------------------
+// --------------------------------------------------------------
 // requisite functions below
 
 function createCircle() {
-    // while (Date.now() - initTime < 10000) {let wait=1;}
-    let circle = {"obj" : $("<div></div>"), "createTime" : Date.now()};
-    // console.log(circle.createTime - initTime);
+    let circle = {"obj" : $("<div></div>"), "createTime" : Date.now(), "clicked" : false};
     circle.obj.addClass("circle");
-    circle.obj.click((c)=>{handleClick(c);})
+    // 'mousedown' event prevents missed clicks during mouse movement
+    circle.obj.mousedown((c)=>{if (!circle.clicked) {circle.clicked=true; handleClick(c);}})
     gameScreen.append(circle.obj);
     
     // gen random nums to translate circle.obj
@@ -71,19 +91,16 @@ function createCircle() {
     circle.obj.css("top", `${screenProps.y + offsetY}px`);
     circles.push(circle);
 
-    function handleClick(circ) {
+    async function handleClick(click) {if (displayTime){ // only allow circles to be clicked if game is running
         score++;
         scoreTxt.text(`Score: ${score}`);
-        let index;
-        for (let i=0;i<circles.length;i++) {
-            if (circ.target == circles[i].obj[0]) {
-                circles.splice(i, 1);
-                circ.target.remove();
-            }
-        }
-    }
+        let c = gameScreen.find(click.target);
+        c.css("animation", "fadeOut 0.25s both");
+        c.css("backgroundColor", "green");
+        await wait(500);
+        c.remove();
+    }}
 }
-
 function updateScreen(){screenProps = {
     "x": parseInt(gameScreen[0].getBoundingClientRect().x),
     "y": parseInt(gameScreen[0].getBoundingClientRect().y),
@@ -110,10 +127,6 @@ async function repeatFunc(callback, interval = 1000, lim = 5) {
         lag.push(interval - (nextTime - Date.now()));
     }
 
-    function wait(ms = 1000) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
     
     // DEBUG LINE - logs smart timer
     // for (let i=0;i<lag.length;i++){console.log(`Run ${i+1} - lag (ms): ${lag[i]}`);}
@@ -121,14 +134,6 @@ async function repeatFunc(callback, interval = 1000, lim = 5) {
     return () => {extCond = false; cond = false};
 }
 
-async function timer(callback, time = 1000) {
-    const initTime = Date.now();
-    // function run() {
-    await wait(time);
-    callback;
-    console.log(`timer lag: ${Date.now() - initTime - time}`);
-    function wait(ms = 1000) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    return Date.now() - initTime - time;
+function wait(ms = 1000) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
